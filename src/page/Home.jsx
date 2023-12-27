a94e12644917183059464300e17becf437c743b0
himaloyash <himaloyashis98@gmail.com>
2023-12-27 22:25:14 +0600

    slider done

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
+            <p className='text-3xl text-cyan-400'>Here is home for slider byte</p>
         </div>
     );
 };
