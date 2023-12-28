86e82e60c838439e5bfb7d3556723d9c739fd223
himaloyash <himaloyashis98@gmail.com>
2023-12-28 16:20:06 +0600

    data faced

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 
 import "./slide.css"
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
 
 const Slider = () => {
+
+
+    const { data: products = [] } = useQuery({
+        queryKey: ["AllProduct"],
+        queryFn: async () => {
+            const res = await axios("product.json");
+            console.log({res});
+            return res?.data;
+        }
+    })
+
+    console.log(products);
+
     return (
         <swiper-container>
-            <swiper-slide>
-                
-            </swiper-slide>
+
+            {
+                products.map(product => {
+                    <swiper-slide>
+                        <p>title {product.title}</p>
+                    </swiper-slide>
+                })
+            }
+
         </swiper-container>
     );
 };
