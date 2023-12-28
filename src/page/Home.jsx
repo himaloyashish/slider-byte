7cbeb94896b6982c9b9243e771687944abce37d3
himaloyash <himaloyashis98@gmail.com>
2023-12-28 12:20:21 +0600

    slide changed

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
+// import React from 'react';
+
+// import { EffectFade, Navigation, Pagination } from 'swiper/modules';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import 'swiper/swiper.scss'; // core Swiper
+// import 'swiper/modules/navigation.scss'; // Navigation module
+// import 'swiper/modules/pagination.scss'; // Pagination module
 
 const Home = () => {
     return (
-        <div>
-            <p className='text-3xl text-cyan-400'>Here is home for slider byte</p>
-        </div>
+        // <>
+        //     <Swiper
+        //         spaceBetween={30}
+        //         effect={'fade'}
+        //         navigation={true}
+        //         // pagination={{
+        //         //     clickable: true,
+        //         // }}
+        //         modules={[EffectFade, Navigation, Pagination]}
+        //         className="mySwiper"
+        //     >
+        //         <SwiperSlide className='  bg-[#F3CCF3] h-auto w-full' >
+        //             {/* <div className='slide'>
+                       
+        //             </div> */}
+        //             <div className='grid lg:grid-cols-3 h-full  ' >
+        //                 <img className='w-full ' src="https://img.freepik.com/free-photo/light-forest-day-anime-background-illustration-generative-ai_1258-150804.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=sph" />
+        //                 <img src="https://img.freepik.com/free-photo/beautiful-shot-forest-with-tall-green-trees_181624-20615.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=sph" alt="" />
+        //                 <img src="https://img.freepik.com/free-photo/beautiful-shot-pathway-middle-field-countryside_181624-7267.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=sph" alt="" />
+        //             </div>
+        //         </SwiperSlide>
+        //         <SwiperSlide>
+        //             <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        //         </SwiperSlide>
+        //         <SwiperSlide>
+        //             <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        //         </SwiperSlide>
+        //         <SwiperSlide>
+        //             <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        //         </SwiperSlide>
+        //     </Swiper>
+        //     <div>
+        //         <p>ok</p>
+        //     </div>
+        // </>
+        ''
     );
 };
 
