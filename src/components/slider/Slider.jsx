b727875a8669db0331ccd7e898f56ab309a7ebaf
himaloyash <himaloyashis98@gmail.com>
2023-12-28 19:41:01 +0600

    offer slider created

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -12,7 +12,7 @@ const Slider = () => {
         queryKey: ["AllProduct"],
         queryFn: async () => {
             const res = await axios("product.json");
-
+            
             return res?.data;
         }
     })
@@ -20,12 +20,12 @@ const Slider = () => {
     console.log(products);
 
     return (
-        <swiper-container slides-per-view="3" navigation="true" space-between='20' loop='true'>
+        <swiper-container slides-per-view="2" navigation="true" space-between='20' loop='true'>
             <swiper-slide >
                 <img className='w-1/3' src="https://img.freepik.com/free-photo/television-houseplants-room-scene-generative-ai_188544-12145.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=ais" alt="product_1" />
-                <div>
+                <div className='p-2'>
                     <p>4.4</p>
-                    <p>LG Monitor 43xn</p>
+                    <p className='text-xl font-bold'>LG Monitor 43xn</p>
                     <p>$500</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, recusandae.</p>
 
@@ -33,9 +33,9 @@ const Slider = () => {
             </swiper-slide>
             <swiper-slide>
                 <img className='w-1/3' src="https://img.freepik.com/free-photo/landscape-nature-scene-tv-appliance-generative-ai_188544-12122.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=ais" alt="product_2" />
-                <div>
+                <div className='p-2'>
                     <p>4.5</p>
-                    <p>Samsung m20</p>
+                    <p className='text-xl font-bold'>Samsung m20</p>
                     <p>$5550</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, recusandae.</p>
 
@@ -43,9 +43,9 @@ const Slider = () => {
             </swiper-slide>
             <swiper-slide>
                 <img className='w-1/3' src="https://img.freepik.com/free-photo/television-with-candles-room-generative-ai_188544-19471.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=ais" alt="product_3" />
-                <div>
+                <div className='p-2'>
                     <p>4.4</p>
-                    <p>Samsung m20 pro</p>
+                    <p className='text-xl font-bold'>Samsung m20 pro</p>
                     <p>$680</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, recusandae.</p>
 
@@ -53,9 +53,9 @@ const Slider = () => {
             </swiper-slide>
             <swiper-slide>
                 <img className='w-1/3' src="https://img.freepik.com/free-psd/computer-mockup_1310-706.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=ais" alt="product_4" />
-                <div>
+                <div className='p-2'>
                     <p>4.5</p>
-                    <p>HP Elite Computer</p>
+                    <p className='text-xl font-bold'>HP Elite Computer</p>
                     <p>$1280</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, recusandae.</p>
 
@@ -63,9 +63,9 @@ const Slider = () => {
             </swiper-slide>
             <swiper-slide>
                 <img className='w-1/3' src="https://img.freepik.com/free-photo/technology-electronic-devices_144627-10803.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=ais" alt="product_5" />
-                <div>
+                <div className='p-2'>
                     <p>4.7</p>
-                    <p>HP ZenBook Laptop</p>
+                    <p className='text-xl font-bold'>HP ZenBook Laptop</p>
                     <p>$1480</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, recusandae.</p>
 
@@ -73,9 +73,9 @@ const Slider = () => {
             </swiper-slide>
             <swiper-slide>
                 <img className='w-1/3' src="https://img.freepik.com/free-photo/closeup-digital-camera-with-lenses_181624-59757.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=sph" alt="product_6" />
-                <div>
+                <div className='p-2'>
                     <p>4.7</p>
-                    <p>Nikon g43</p>
+                    <p className='text-xl font-bold'>Nikon g43</p>
                     <p>$1780</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, recusandae.</p>
 
@@ -83,9 +83,9 @@ const Slider = () => {
             </swiper-slide>
             <swiper-slide>
                 <img className='w-1/3' src="https://img.freepik.com/free-photo/top-view-photography-accesories-with-copy-space_23-2148363427.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=sph" alt="product_7" />
-                <div>
+                <div className='p-2'>
                     <p>4.7</p>
-                    <p>Nikon g43 Pro</p>
+                    <p className='text-xl font-bold'>Nikon g43 Pro</p>
                     <p>$1680</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, recusandae.</p>
 
@@ -93,9 +93,9 @@ const Slider = () => {
             </swiper-slide>
             <swiper-slide>
                 <img className='w-1/3' src="https://img.freepik.com/free-photo/top-view-photography-accesories-with-copy-space_23-2148363427.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=sph" alt="product_8" />
-                <div>
+                <div className='p-2'>
                     <p>4.7</p>
-                    <p>Canon g5</p>
+                    <p className='text-xl font-bold'>Canon g5</p>
                     <p>$1680</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, recusandae.</p>
 
---
+++ src/components/slider2/OfferSlider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OfferSlider = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default OfferSlider;
