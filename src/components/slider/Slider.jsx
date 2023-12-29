f66b92405db02b68a06e506781a40d73aeb70bfe
himaloyash <himaloyashis98@gmail.com>
2023-12-29 11:49:51 +0600

    done

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,28 +1,17 @@
 import React from 'react';
 
 import "./slide.css"
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 
 
 const Slider = () => {
 
 
-    const { data: products = [] } = useQuery({
-        queryKey: ["AllProduct"],
-        queryFn: async () => {
-            const res = await axios("product.json");
-
-            return res?.data;
-        }
-    })
-
-    console.log(products);
+   
 
     return (
         <swiper-container slides-per-view="" navigation="true" space-between='20' loop='true'>
             <swiper-slide >
-                <div className='p-2 gap-3 md:flex justify-center items-center'>
+                <div className='p-2 gap-3 md:flex justify-center relative items-center'>
                     <img className='w-1/3' src="https://img.freepik.com/free-photo/television-houseplants-room-scene-generative-ai_188544-12145.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=ais" alt="product_1" />
                     <div>
                         <p>4.4</p>
@@ -30,9 +19,10 @@ const Slider = () => {
                         <p className='text-red-600'>$500</p>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, recusandae.</p>
                     </div>
+                    <span className='absolute w-12 h-12 rounded-3xl top-4 left-5 p-1 bg-orange-400'>-8%</span>
 
                 </div>
-                <div className='p-2 gap-3 md:flex justify-center items-center'>
+                <div className='p-2 gap-3 md:flex relative justify-center items-center'>
                     <img className='w-1/3' src="https://img.freepik.com/free-photo/landscape-nature-scene-tv-appliance-generative-ai_188544-12122.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=ais" alt="product_2" />
                     <div>
                         <p>4.5</p>
@@ -43,10 +33,11 @@ const Slider = () => {
                         </div>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, recusandae.</p>
                     </div>
+                    <span className='absolute w-12 h-12 rounded-3xl top-4 left-5 p-1 bg-orange-400'>-10%</span>
 
                 </div>
 
-                <div className='p-2 gap-3 md:flex justify-center items-center'>
+                <div className='p-2 gap-3 md:flex relative justify-center items-center'>
                     <img className='w-1/3' src="https://img.freepik.com/free-photo/television-with-candles-room-generative-ai_188544-19471.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=ais" alt="product_3" />
                     <div>
                         <p>4.4</p>
@@ -58,8 +49,9 @@ const Slider = () => {
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, recusandae.</p>
                     </div>
 
+                    <span className='absolute w-12 h-12 rounded-3xl top-4 left-5 p-1 bg-orange-400'>-15%</span>
                 </div>
-                <div className='p-2 gap-3 md:flex justify-center items-center'>
+                <div className='p-2 gap-3 md:flex relative justify-center items-center'>
                     <img className='w-1/3' src="https://img.freepik.com/free-psd/computer-mockup_1310-706.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=ais" alt="product_4" />
                     <div>
                         <p>4.6</p>
@@ -70,7 +62,7 @@ const Slider = () => {
                         </div>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, recusandae.</p>
                     </div>
-
+                    <span className='absolute w-12 h-12 rounded-3xl top-4 left-5 p-1 bg-orange-400'>-6%</span>
                 </div>
             </swiper-slide>
 
@@ -88,7 +80,7 @@ const Slider = () => {
                     </div>
 
                 </div>
-                <div className='p-2 gap-3 flex justify-center items-center'>
+                <div className='p-2 gap-3 relative flex justify-center items-center'>
                     <img className='w-1/3' src="https://img.freepik.com/free-photo/closeup-digital-camera-with-lenses_181624-59757.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=sph" alt="product_6" />
                     <div>
                         <p>4.5</p>
@@ -99,9 +91,9 @@ const Slider = () => {
                         </div>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, recusandae.</p>
                     </div>
-
+                    <span className='absolute w-12 h-12 rounded-3xl top-4 left-5 p-1 bg-orange-400'>-10%</span>
                 </div>
-                <div className='p-2 gap-3 flex justify-center items-center'>
+                <div className='p-2 gap-3 flex relative justify-center items-center'>
                     <img className='w-1/3' src="https://img.freepik.com/free-photo/top-view-photography-accesories-with-copy-space_23-2148363427.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=sph" alt="product_7" />
                     <div>
                         <p>4.9</p>
@@ -112,7 +104,7 @@ const Slider = () => {
                         </div>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, recusandae.</p>
                     </div>
-
+                    <span className='absolute w-12 h-12 rounded-3xl top-4 left-5 p-1 bg-orange-400'>-10%</span>
                 </div>
                 <div className='p-2 gap-3 flex justify-center items-center'>
                     <img className='w-1/3' src="https://img.freepik.com/free-photo/top-view-photography-accesories-with-copy-space_23-2148363427.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=sph" alt="product_8" />
--- src/components/slider2/OfferSlider.jsx
+++ src/components/slider2/OfferSlider.jsx
@@ -1,13 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 const OfferSlider = () => {
+
+    const [category, setCategory] = useState('television');
+  
+
+    const { data: products = [], refetch } = useQuery({
+        queryKey: ["AllProduct"],
+        queryFn: async () => {
+            const res = await axios(`https://byte-ss.vercel.app/device/${category}`);
+            
+            return res?.data;
+        }
+    })
+    
+    if(category === 'television' || 'laptop'|| 'camera'){
+        refetch()
+    }
+    refetch()
+
+
+    console.log(products);
+
     return (
-        <div>
-            <div>
-                <img src="https://img.freepik.com/free-vector/modern-super-sale-promotion-bright-banner_1055-6985.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=ais" alt="Offer" />
+        <div className='md:flex justify-between  gap-3 w-full px-10 bg-[#FAE7F3] pt-10'>
+            <div className='w-2/3'>
+                <img className='' src="https://img.freepik.com/free-vector/modern-super-sale-promotion-bright-banner_1055-6985.jpg?size=626&ext=jpg&uid=R124603556&ga=GA1.1.152808567.1700245140&semt=ais" alt="Offer" />
             </div>
-            <div>
-                
+            <div className=' md:w-1/3 bg-slate-400'>
+                <Tabs className={''}>
+                    <TabList>
+                        <Tab onClick={()=>setCategory('television')}><p className='h-3 w-3 rounded-md bg-orange-300'></p></Tab>
+                        <Tab onClick={()=>setCategory('laptop')} ><p className='h-3 w-3 rounded-md bg-orange-300'></p></Tab>
+                        <Tab onClick={()=>setCategory('camera')}><p className='h-3 w-3 rounded-md bg-orange-300'></p></Tab>
+                    </TabList>
+
+                    <TabPanel>
+                        <div className='bg-[#]  py-2 mb-2'>
+                            {
+                                products.map(product=> (
+                                    <div className='lg:flex md:flex w-full justify-center gap-2 items-center mb-2'>
+                                        <img className='md:w-1/3 object-fill' src={product.image} alt="img1" />
+                                        <div>
+                                            <p className='font-bold'>{product.rating}</p>
+                                            <p className=''>{product.title}</p>
+                                            <p className=''>$ {product.price}</p>
+                                        </div>
+                                    </div>
+                                
+                                ))
+                            }
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className='bg-[#]  py-2 mb-2'>
+                            {
+                                products.map(product=> (
+                                    <div className='lg:flex md:flex w-full justify-center gap-2 items-center mb-2'>
+                                        <img className='md:w-1/3 object-fill' src={product.image} alt="img1" />
+                                        <div>
+                                            <p className='font-bold'>{product.rating}</p>
+                                            <p className=''>{product.title}</p>
+                                            <p className=''>$ {product.price}</p>
+                                        </div>
+                                    </div>
+                                
+                                ))
+                            }
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className='bg-[#]  py-2 mb-2'>
+                            {
+                                products.map(product=> (
+                                    <div className='lg:flex md:flex w-full justify-center gap-2 items-center mb-2'>
+                                        <img className='md:w-1/3 object-fill' src={product.image} alt="img1" />
+                                        <div>
+                                            <p className='font-bold'>{product.rating}</p>
+                                            <p className=''>{product.title}</p>
+                                            <p className=''>$ {product.price}</p>
+                                        </div>
+                                    </div>
+                                
+                                ))
+                            }
+                        </div>
+                    </TabPanel>
+                 
+                </Tabs>
             </div>
         </div>
     );
